343945c90e047398b176767775c21fc4305bb234
bigblesslee <31499353+bigblesslee@users.noreply.github.com>
2017-08-30 23:01:21 -0700

    Update taxi_MonHour.R

--- taxi_MonHour.R
+++ taxi_MonHour.R
@@ -7,9 +7,9 @@ month = date$mon
 hour = date$hour
 
 mhd = data.frame(taxi_tmp$trip_duration,month,hour)
-
-plot(mhd$hour,mhd$taxi_tmp.trip_duration)
 colnames(mhd) = c("trip_duration","month","hour")
+plot(hour,trip_duration)
+
 library(plyr)
 h_am =ddply(mhd,"hour",summarise,total.count=sum(1))
 plot(h_d)
