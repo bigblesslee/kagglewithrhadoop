d7e2ebd30c7de4ac8f50b3ef8ee5ff80c5fa161b
bigblesslee <31499353+bigblesslee@users.noreply.github.com>
2017-08-30 23:18:17 -0700

    Update location.R

--- location.R
+++ location.R
@@ -3,4 +3,8 @@ drop_location = data.frame(taxi_tmp$id, taxi_tmp$dropoff_latitude, taxi_tmp$drop
 colnames(pick_location) = c("id","p_latitude","p_longtitude")
 colnames(drop_location) = c("id","d_latitude","d_longtitude")
 
-plot(pick_location$p_latitude,pick_location$p_longtitude)
+pick_location_out = subset(pick_location,pick_location$p_latitude < 50)
+pick_location_out2 = subset(pick_location, pick_location$p_longtitude>-100)
+
+plot(pick_location_out2$p_latitude,pick_location$p_longtitude)
+
